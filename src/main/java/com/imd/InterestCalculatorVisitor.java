c225cbdf734881cad6b3558701b9478b260164d8
Maycon Douglas <mdrs.maycon@gmail.com>
2024-11-04 15:26:55 -0300

    update

--- src/main/java/com/imd/InterestCalculatorVisitor.java
+++ src/main/java/com/imd/InterestCalculatorVisitor.java
@@ -7,27 +7,24 @@ import com.imd.models.SavingsAccount;
 import java.util.List;
 
 public class InterestCalculatorVisitor implements Visitor {
-    private double interestRate;
-
+    private final double interestRate;
     public void interestCalculator(List<Account> accounts) {
         for (Account account : accounts) {
             account.accept(this);
         }
     }
-
     public InterestCalculatorVisitor(double interestRate) {
         this.interestRate = interestRate;
     }
-
     @Override
     public void visit(CurrentAccount currentAccount) {
         double interest = currentAccount.getBalance() * (interestRate / 100);
         System.out.println("Juros para Conta Corrente: " + interest);
     }
-
     @Override
     public void visit(SavingsAccount savingsAccount) {
         double interest = savingsAccount.getBalance() * ((interestRate + 2) / 100);
         System.out.println("Juros para Conta Poupança: " + interest);
     }
 }
+
--- src/main/java/com/imd/models/CurrentAccount.java
+++ src/main/java/com/imd/models/CurrentAccount.java
@@ -3,12 +3,10 @@ package com.imd.models;
 import com.imd.Visitor;
 
 public class CurrentAccount implements Account {
-    private double balance;
-
+    private final double balance;
     public CurrentAccount(double balance) {
         this.balance = balance;
     }
-
     public double getBalance() {
         return balance;
     }
